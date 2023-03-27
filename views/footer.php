411bc316ae20203c88da49b7d0c1ba61bcd0d217
sami abdulhalim <59802097+aboalsim114@users.noreply.github.com>
2023-03-27 16:11:05 +0200

    correction css

--- views/footer.php
+++ views/footer.php
@@ -1,11 +1,11 @@
 
 <body class="d-flex flex-column min-vh-100">
 <footer style="background-color: #235" class="bg text-white text-center text-lg-start mt-auto ">
-  <!-- Grid container -->
+
   <div class="container p-4">
-    <!--Grid row-->
+
     <div class="row">
-      <!--Grid column-->
+
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
         <h5 class="text-uppercase">Ecebook</h5>
 
@@ -13,9 +13,8 @@
         EceBook est une plateforme conviviale et intuitive qui permet aux étudiants de rester en contact avec leurs amis et leur communauté de l'ECE Paris, même lorsqu'ils sont loin de l'école. C'est une plateforme idéale pour les étudiants qui cherchent à élargir leur réseau et à rester connectés avec leurs pairs.
         </p>
       </div>
-      <!--Grid column-->
 
-      <!--Grid column-->
+
       <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
                     <h5 class="text-uppercase mb-0 text-primary">Nous contacter</h5>
                     <ul class="list-unstyled">
@@ -35,21 +34,18 @@
                         </li>
                     </ul>
                 </div>
-      <!--Grid column-->
-
+      
    
-      <!--Grid column-->
     </div>
-    <!--Grid row-->
+ 
   </div>
-  <!-- Grid container -->
+ 
 
-  <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © <?= date("Y")  . " " ?>Copyright:
     <a class="text-white" href="https://mdbootstrap.com/">ECEBOOK</a>
   </div>
-  <!-- Copyright -->
+
 </footer>
 
 </body>
